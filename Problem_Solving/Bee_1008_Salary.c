5b9dcde1a663d1eb8b85fc69648f325625e6ac70
mohammed_051 <117919639+Mohammed-051@users.noreply.github.com>
2023-02-09 19:52:10 +0600

    Add files via upload

---
+++ Problem_Solving/Bee_1008_Salary.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+  double A,B,C,D,X;
+
+  scanf("%lf %lf %lf", &A,&B,&C);
+    X=B*C;
+
+  printf("NUMBER = %.0lf\n", A);
+  printf("SALARY = U$ %.2lf\n", X);
+
+  return 0;
+}
