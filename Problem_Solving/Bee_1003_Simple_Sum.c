1030bbbfefbea354b3f4a136248b0fff6d17b268
mohammed_051 <117919639+Mohammed-051@users.noreply.github.com>
2023-02-09 23:48:03 +0600

    Update and rename Bee_1003_Simple_Product.c to Bee_1003_Simple_Sum.c

---
+++ Problem_Solving/Bee_1003_Simple_Sum.c
@@ -0,0 +1,16 @@
+
+/*Read two integer values, in this case, the variables A and B. 
+After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.*/
+#include<stdio.h>
+int main(void){
+    int A,B,SOMA;
+
+    scanf("%d %d",&A,&B);
+
+     SOMA = A+B;
+
+    printf("SOMA =%d\n",SOMA);
+
+
+  return 0;
+}
